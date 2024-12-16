8bbabbbeaca738ea403fc1e6284700c3ac66c200
Siddhi <majrekarsiddhi121@gmail.com>
2024-12-16 18:18:01 +0530

    spotify-clone

---
+++ src/components/AlbumItem.jsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+
+import { useNavigate } from "react-router-dom"
+import PropTypes from 'prop-types';
+
+const AlbumItem = ({ image, name, desc, id }) => {
+
+    const navigate = useNavigate()
+
+    return (
+        <div onClick={()=>navigate(`/album/${id}`)} className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+            <img className="rounded" src={image} alt="" />
+            <p className="font-bold mt-2 mb-1">{name}</p>
+            <p className="text-slate-200 text-sm">{desc}</p>
+        </div>
+    )
+}
+
+AlbumItem.propTypes = {
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+};
+
+export default AlbumItem
